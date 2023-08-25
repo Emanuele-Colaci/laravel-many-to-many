cb00b024acb573d72e49ca44d1c492da08dc0e83
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-25 16:44:57 +0200

    relazione many to many fatta

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -16,4 +17,8 @@ class Project extends Model
         return $this->belongsTo(Type::class);
     }
 
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
